84290dc681d0b8f2c1c8e2ef538c55173ec7c92b
yikeyatu <liangchen005005@163.com>
2015-03-04 23:43:09 +0800

    s

--- lo/src.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "connect.php";
-$name =$_GET['name'];
-$sql = "selcect *from video where Video_Name = '%" .$name. "%' "; 
-foreach($dbh->query($sql)as$row){
-	echo $row['Video_Address'];
-}
-?>
